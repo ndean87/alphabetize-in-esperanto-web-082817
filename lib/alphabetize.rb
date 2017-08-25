fe0ee905b519fc28f18ed3412b3ca4d2155036e7
Natalie Dean <ndean87@gmail.com>
2017-08-25 13:26:45 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
+require "pry"
+
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  sorted_array = arr.sort_by do |string|
+      string.tr(esperanto_alphabet, alphabet)
+  end
+  sorted_array
 end
+
+#"bonan vesperon".tr(esperanto_alphabet, alphabet)
+
+# ["bonan vesp", "bonan matenonabcsder", "bonan nokton"].sort_by { |string|  string.tr(esperanto_alphabet, alphabet)
+#  }
